8eddc220a5a13b03928f9df1a01d7916ea079fc5
tugcekrrgz <123090853+tugcekrrgz@users.noreply.github.com>
2023-08-08 10:51:23 +0300

    SRP_Employee
    employee ekleme, listeleme

---
+++ Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP_Employee
+{
+    internal class Menu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Validation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP_Employee
+{
+    internal class Validation
+    {
+        public static bool Validate(Employee employee)
+        {
+            if (string.IsNullOrEmpty(employee.Firstname))
+            {
+                Message.ErrorMessage("İsim");
+                return false;
+            }
+            if (string.IsNullOrEmpty(employee.Lastname))
+            {
+                Message.ErrorMessage("Soyad");
+                return false;
+            }
+            return true;
+        }
+    }
+}
